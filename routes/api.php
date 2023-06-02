1e623c77fc2db74eba48ea26076e861ce692b0bd
sanyath007 <sanyath007@gmail.com>
2023-06-02 14:34:21 +0700

    Create new EquipmentController and Equipment model

--- routes/api.php
+++ routes/api.php
@@ -18,19 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/db-connection', function () {
-    try {
-        $dbconnect = \DB::connection()->getPDO();
-        $dbname = \DB::connection()->getDatabaseName();
-
-        echo "Connected successfully to the database. Database name is :".$dbname;
-    } catch(Exception $e) {
-        echo $e->getMessage();
-    }
-});
-
-Route::get('/contents', 'App\Http\Controllers\WebDataController@getAll');
-
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function() {
     Route::post('register', 'App\Http\Controllers\AuthController@register');
     Route::post('login', 'App\Http\Controllers\AuthController@login');
@@ -38,3 +25,20 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function() {
     Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
     Route::post('me', 'App\Http\Controllers\AuthController@me');
 });
+
+Route::middleware('auth:api')->group(function() {
+    Route::get('/db-connection', function () {
+        try {
+            $dbconnect = \DB::connection()->getPDO();
+            $dbname = \DB::connection()->getDatabaseName();
+    
+            echo "Connected successfully to the database. Database name is :".$dbname;
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+    });
+
+    Route::get('/contents', 'App\Http\Controllers\WebDataController@getAll');
+    Route::get('/equipments', 'App\Http\Controllers\EquipmentController@getAll');
+});
+
