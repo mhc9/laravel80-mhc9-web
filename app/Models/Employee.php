4da35d7de40416abe6e7a4040e2b54439f9dce3e
sanyath007 <sanyath007@gmail.com>
2023-06-08 20:52:15 +0700

    Create new Position and Class model and add relationships in Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -10,4 +10,19 @@ class Employee extends Model
     // protected $primaryKey = 'id';
     // public $incrementing = false; // false = ไม่ใช้ options auto increment
     // public $timestamps = false; // false = ไม่ใช้ field updated_at และ created_at
+
+    public function prefix()
+    {
+        return $this->belongsTo(Prefix::class, 'prefix_id', 'id');
+    }
+
+    public function position()
+    {
+        return $this->belongsTo(Position::class, 'position_id', 'id');
+    }
+
+    public function class()
+    {
+        return $this->belongsTo(Class::class, 'class_id', 'id');
+    }
 }
