d7d91cc1e61bf91e41b6d5d1a90b2aed392e6465
sanyath007 <sanyath007@gmail.com>
2023-07-04 14:54:13 +0700

    Create new models

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tambon extends Model
+class Activity extends Model
 {
-    protected $table = 'tambons';
+    protected $table = 'activities';
     // protected $primaryKey = 'id';
     // public $incrementing = false; // false = ไม่ใช้ options auto increment
     // public $timestamps = false; // false = ไม่ใช้ field updated_at และ created_at
--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tambon extends Model
+class Plan extends Model
 {
-    protected $table = 'tambons';
+    protected $table = 'plans';
     // protected $primaryKey = 'id';
     // public $incrementing = false; // false = ไม่ใช้ options auto increment
     // public $timestamps = false; // false = ไม่ใช้ field updated_at และ created_at
