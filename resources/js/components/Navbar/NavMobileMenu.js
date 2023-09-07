152c2bbcaea1e2ae955a928497c0a620ebb31ecd
sanyath007 <sanyath007@gmail.com>
2023-09-07 09:27:33 +0700

    Create new NavMobileMenu component and add its to Navbar

---
+++ resources/js/components/Navbar/NavMobileMenu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavMobileMenu = () => {
+    return (
+        <div className="mobile-menu">
+            <div className="sidenav-btn">
+                <div className="hamburger">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavMobileMenu
