7bfe24a484ab84b6b5b2517402e42fa1d0dbd14c
sanyath007 <sanyath007@gmail.com>
2023-09-09 17:39:20 +0700

    Re-design PostList

--- resources/js/components/Post/List.js
+++ resources/js/components/Post/List.js
@@ -9,18 +9,26 @@ const PostList = () => {
 
             <div className="post-list">
                 <div className="row">
-                    <div className="col-md-7 post-headline">
-                        <div className="headline-img">
-                            <img src="https://cdn.pixabay.com/photo/2023/01/24/10/02/woman-7740613_960_720.jpg" alt="" />
+                    <div className="col-md-7">
+                        <div className="post-headline">
+                            <div className="headline-img">
+                                <img src="https://cdn.pixabay.com/photo/2023/01/24/10/02/woman-7740613_960_720.jpg" alt="" />
+                            </div>
+                            <div className="headline-text">
+                                <h4>สุดปัง! คว้ารางวัลคุณภาพบริหารจัดการภาครัฐดีเด่น</h4>
+                                <h5 className="text-muted">
+                                    <span><i class="fas fa-calendar-alt"></i> 2023-09-09 16:00 น.</span>
+                                    <span><i class="fas fa-user-alt"></i> Admin</span>
+                                </h5>
+                                <p>
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                    <span className="readmore"><a href="">อ่านเพิ่มเติม</a></span>
+                                </p>
+                            </div>
                         </div>
-                        <h4>สุดปัง! คว้ารางวัลคุณภาพบริหารจัดการภาครัฐดีเด่น</h4>
-                        <p>
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                            <span className="readmore"><a href="">อ่านเพิ่มเติม</a></span>
-                        </p>
                     </div>
                     <div className="col-md-5">
-                        <ul>
+                        <ul className="post-secondary">
                             {[1,2,3].map((item, index) => (
                                 <li>
                                     <div className="post__secondary-img">
@@ -28,6 +36,10 @@ const PostList = () => {
                                     </div>
                                     <div className="post__secondary-text">
                                         <h4>สุดปัง! คว้ารางวัลคุณภาพบริหารจัดการภาครัฐดีเด่น</h4>
+                                        <h5 className="text-muted">
+                                            <span><i class="fas fa-calendar-alt"></i> 2023-09-09 16:00 น.</span>
+                                            <span><i class="fas fa-user-alt"></i> Admin</span>
+                                        </h5>
                                         <p>
                                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
                                             <span className="readmore"><a href="">อ่านเพิ่มเติม</a></span>
@@ -36,8 +48,7 @@ const PostList = () => {
                                 </li>
                             ))}
                         </ul>
-
-                        <div style={{ textAlign: 'center', margin: '0' }}>
+                        <div className="post-btn">
                             <a href="#" className="all-news">ทั้งหมด <i class="fas fa-angle-double-right"></i></a>
                         </div>
                     </div>
