caf3dc50bc10bd524b6b83be34401e6f46f596b3
sanyath007 <sanyath007@gmail.com>
2023-09-10 11:53:48 +0700

    Modify VideoList by adding videos const to VideoList and render its as video component instead of head coding

--- resources/js/components/Video/List.js
+++ resources/js/components/Video/List.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+const videos = [
+    { id: 1, title: '3 รู้...อยู่...รอด อำเภอเนินสง่า จังหวัดชัยภูมิ', description: '', url: 'https://www.youtube.com/embed/b5YsSxrnZb8' },
+    { id: 2, title: 'สูงวัยพลังใจสร้างได้', description: '', url: 'https://www.youtube.com/embed/OQo3keOSJyU' },
+    { id: 3, title: '3 รู้...อยู่...รอด อำเภอเนินสง่า จังหวัดชัยภูมิ', description: '', url: 'https://www.youtube.com/embed/b5YsSxrnZb8' },
+    { id: 4, title: 'สูงวัยพลังใจสร้างได้', description: '', url: 'https://www.youtube.com/embed/OQo3keOSJyU' },
+];
+
 const VideoList = () => {
     return (
         <div className="video-box">
@@ -7,8 +14,24 @@ const VideoList = () => {
 
             <hr className="my-2" />
 
-            <div className="video-list">
-                
+            <div className="row video-list">
+                {videos.map((video, index) => (
+                    <div className="col-md-6" key={video.id}>
+                        <div className="video-item">
+                            <iframe
+                                width="100%"
+                                height="100%"
+                                src={video.url}
+                                title={video.title}
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="video-btn">
+                <a href="#" className="all-news">วิดีโอทั้งหมด <i class="fas fa-angle-double-right"></i></a>
             </div>
         </div>
     )
