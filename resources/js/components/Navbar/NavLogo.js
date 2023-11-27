092de1faf31ac209ddfa7806e93200edd07e0876
sanyath007 <sanyath007@gmail.com>
2023-11-27 14:26:16 +0700

    Use Link react-router-dom component instead of a tag in NavLogo

--- resources/js/components/Navbar/NavLogo.js
+++ resources/js/components/Navbar/NavLogo.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavLogo = () => {
     return (
-        <a href="/" className="nav-logo">
+        <Link to="/" className="nav-logo">
             <picture className="logo">
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/f/f9/%E0%B8%95%E0%B8%A3%E0%B8%B2%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%87%E0%B8%AA%E0%B8%B2%E0%B8%98%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88.png"
@@ -13,7 +14,7 @@ const NavLogo = () => {
                 <h2>ศูนย์สุขภาพจิตที่ 9</h2>
                 <p>Mental Health Center 9</p>
             </div>
-        </a>
+        </Link>
     )
 }
 
