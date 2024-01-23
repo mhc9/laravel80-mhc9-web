d37f9d57b6a2ad12a00913e630cfb384e9bac352
sanyath007 <sanyath007@gmail.com>
2024-01-23 12:37:12 +0700

    reflactor(AnnualReports.js): re-design

--- resources/js/components/pages/AnnualReports.js
+++ resources/js/components/pages/AnnualReports.js
@@ -2,13 +2,24 @@ import React from 'react'
 
 function AnnualReports() {
     return (
-        <section className="report container">
+        <section className="policies container">
             <h1 className="title">รายงานประจำปี</h1>
 
             <hr className="my-2" />
 
-            <div className="report-wrapper">
-
+            <div className="policies-wrapper">
+                <a href="#">
+                    <div className="policy">
+                        <h3>รายงานประจำปี 2566</h3>
+                        <span><i className="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="#">
+                    <div className="policy">
+                        <h3>รายงานประจำปี 2565</h3>
+                        <span><i className="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
             </div>
         </section>
     )
