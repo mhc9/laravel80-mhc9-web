06946d41f7f8a93cbc93c3d017671bbbb9b42f42
sanyath007 <sanyath007@gmail.com>
2024-01-24 17:07:29 +0700

    reflactor: add list of procedure to Procedures component and re-design NewsDetail component

--- resources/js/components/pages/News/Detail.js
+++ resources/js/components/pages/News/Detail.js
@@ -24,25 +24,44 @@ const NewsDetail = () => {
 
             <hr className="my-2" />
 
-            <div className="px-2">
+            <div className="px-2 pb-4">
                 <div className="text-muted text-xs font-thin flex justify-between items-center mb-2 px-2">
                     <span><i className="fas fa-calendar-alt"></i> {news?.publish_up} น.</span>
                     <span><i className="fas fa-user-alt"></i> {news?.author?.name}</span>
                 </div>
 
-                {/* <div className="p-2 mt-4 flex justify-center">
-                    <div className="w-3/5 rounded-md overflow-hidden">
-                        <img src={`./${article?.featured}`} alt="headline-pic" />
-                    </div>
-                </div> */}
+                {/* Render content */}
+                {(news?.content_type_id === 1 && news?.intro_text) && (
+                    <>
+                        <div className="p-2 text-lg">
+                            <span>{news?.intro_text}</span>
+                            {/* <span dangerouslySetInnerHTML={{ __html: news?.full_text }}></span> */}
+                        </div>
 
-                {/* <div className="p-2 text-lg">
-                    <span>{news?.intro_text}</span> */}
-                    {/* <span dangerouslySetInnerHTML={{ __html: news?.full_text }}></span> */}
-                {/* </div> */}
+                        <div className="flex justify-between items-center pt-2 pb-4">
+                            <div className="w-1/2">
+                                {news?.tags && (
+                                    <div className="font-thin">
+                                        {news?.tags.split(',').map((tag, index) => (
+                                            <div className="badge rounded-pill bg-secondary me-1" key={index}>
+                                                {tag}
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
+                            </div>
+                            <div className="flex flex-row gap-3 text-lg">
+                                <i className="far fa-comment"></i>
+                                <i className="far fa-thumbs-up"></i>
+                                <i className="fas fa-heart"></i>
+                                <i className="fas fa-share-square"></i>
+                            </div>
+                        </div>
+                    </>
+                )}
 
                 {/* Render pdf file */}
-                {news?.urls && (
+                {(news?.content_type_id === 2 && news?.urls) && (
                     <div className="px-2 pt-2 pb-4 flex justify-center">
                         <object
                             data={news?.urls}
@@ -55,25 +74,15 @@ const NewsDetail = () => {
                     </div>
                 )}
 
-                {/* <div className="flex justify-between items-center pt-2 pb-4">
-                    <div className="w-1/2">
-                        {news?.tags && (
-                            <div className="font-thin">
-                                {news?.tags.split(',').map((tag, index) => (
-                                    <div className="badge rounded-pill bg-secondary me-1" key={index}>
-                                        {tag}
-                                    </div>
-                                ))}
-                            </div>
-                        )}
+                {/* Render image */}
+                {(news?.content_type_id === 3 && news?.featured) && (
+                    <div className="p-2 mt-4 flex justify-center">
+                        <div className="w-3/5 rounded-md overflow-hidden">
+                            <img src={`./${news?.featured}`} alt="headline-pic" />
+                        </div>
                     </div>
-                    <div className="flex flex-row gap-3 text-lg">
-                        <i className="far fa-comment"></i>
-                        <i className="far fa-thumbs-up"></i>
-                        <i className="fas fa-heart"></i>
-                        <i className="fas fa-share-square"></i>
-                    </div>
-                </div> */}
+                )}
+
             </div>
         </section>
     )
--- resources/js/components/pages/Procedures.js
+++ resources/js/components/pages/Procedures.js
@@ -1,31 +1,68 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Procedures = () => {
     return (
         <section className="policies container">
-            <h1 className="title">นโยบายองค์กร</h1>
+            <h1 className="title">มาตรฐานการปฏิบัติงาน</h1>
 
             <hr className="my-2" />
 
             <div className="policies-wrapper">
-                <a href="#">
+                <Link to="/news/62">
                     <div className="policy">
-                        <h3>นโยบายการคุ้มครองข้อมูลส่วนบุคคล</h3>
+                        <h3>แนวทางการบริหารผลการปฏิบัติงาน</h3>
                         <span><i className="fas fa-angle-down"></i></span>
                     </div>
-                </a>
-                <a href="#">
+                </Link>
+                <Link to="/news/68">
                     <div className="policy">
-                        <h3>นโยบายเว็บไซต์</h3>
+                        <h3>คู่มือการลา</h3>
                         <span><i className="fas fa-angle-down"></i></span>
                     </div>
-                </a>
-                <a href="#">
+                </Link>
+                <Link to="/news/63">
                     <div className="policy">
-                        <h3>นโยบายการรักษาความมั่นคงปลอดภัยเว็บไซต์</h3>
+                        <h3>คู่มือการยืมใช้ทรัพย์สิน</h3>
                         <span><i className="fas fa-angle-down"></i></span>
                     </div>
-                </a>
+                </Link>
+                <Link to="/news/64">
+                    <div className="policy">
+                        <h3>แนวทางการจัดการเรื่องทุจริต</h3>
+                        <span><i className="fas fa-angle-down"></i></span>
+                    </div>
+                </Link>
+                <Link to="/news/66">
+                    <div className="policy">
+                        <h3>ขั้นตอนการจัดซื้อวัสดุ</h3>
+                        <span><i className="fas fa-angle-down"></i></span>
+                    </div>
+                </Link>
+                <Link to="/news/65">
+                    <div className="policy">
+                        <h3>ขั้นตอนการยืมเงินราชการ</h3>
+                        <span><i className="fas fa-angle-down"></i></span>
+                    </div>
+                </Link>
+                <Link to="/news/69">
+                    <div className="policy">
+                        <h3>คู่มือโปรแกรมเดินทางไปราชการ</h3>
+                        <span><i className="fas fa-angle-down"></i></span>
+                    </div>
+                </Link>
+                <Link to="/news/67">
+                    <div className="policy">
+                        <h3>คู่มือการใช้งานระบบเครือข่ายไร้สาย</h3>
+                        <span><i className="fas fa-angle-down"></i></span>
+                    </div>
+                </Link>
+                <Link to="/news/70">
+                    <div className="policy">
+                        <h3>คู่มือการบำรุงรักษาเครื่องคอมพิวเตอร์</h3>
+                        <span><i className="fas fa-angle-down"></i></span>
+                    </div>
+                </Link>
             </div>
         </section>
     )
