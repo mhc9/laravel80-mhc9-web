8c60d70d408163f1cd08c324d08acc1ad7350d58
sanyath007 <sanyath007@gmail.com>
2024-01-25 13:51:44 +0700

    reflactor(PostDetail.js): re-render post data by getting state from global store instead of using local state and render Spinner loading component on data is loading

--- resources/js/components/pages/PostDetail.js
+++ resources/js/components/pages/PostDetail.js
@@ -1,89 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { useDispatch } from 'react-redux';
-import api from '../../api'
-import Spinner from '../Loading/Spinner'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPost } from '../../features/postSlice';
+import Spinner from '../Loading/Spinner';
 import PageHeader from '../PageHeader';
 
 const PostDetail = () => {
     const { id } = useParams();
-    const dispath = useDispatch()
-    const [news, setNews] = useState(null);
+    const dispath = useDispatch();
+    const { post, isLoading } = useSelector(state => state.post);
 
     useEffect(() => {
         if (id) {
-            getPost(id);
+            dispath(getPost(id));
         }
     }, [id]);
 
-    const getPost = async (id) => {
-        const res = await api.get(`/api/posts/${id}`);
-
-        setNews(res.data);
-    };
-
     return (
         <section className="about container">
-            <h1 className="title">{news?.title}</h1>
+            <h1 className="title">{post?.title}</h1>
 
             <hr className="my-2" />
 
             <div className="px-2 pb-4">
-                <PageHeader post={news} />
-
-                {/* Render content */}
-                {(news?.content_type_id === 1 && news?.intro_text) && (
+                {isLoading && <Spinner />}
+                {!isLoading && (
                     <>
-                        <div className="p-2 mt-4 flex justify-center">
-                            <div className="w-3/5 rounded-md overflow-hidden">
-                                <img src={`./${news?.featured}`} alt="headline-pic" />
-                            </div>
-                        </div>
+                        <PageHeader post={post} />
 
-                        <div className="p-2 text-lg">
-                            <span>{news?.intro_text}</span>
-                            {/* <span dangerouslySetInnerHTML={{ __html: news?.full_text }}></span> */}
-                        </div>
-
-                        <div className="flex justify-between items-center pt-2 pb-4">
-                            <div className="w-1/2">
-                                {news?.tags && (
-                                    <div className="font-thin">
-                                        {news?.tags.split(',').map((tag, index) => (
-                                            <div className="badge rounded-pill bg-secondary me-1" key={index}>
-                                                {tag}
-                                            </div>
-                                        ))}
+                        {/* Render content */}
+                        {(post?.content_type_id === 1 && post?.intro_text) && (
+                            <>
+                                <div className="p-2 mt-4 flex justify-center">
+                                    <div className="w-3/5 rounded-md overflow-hidden">
+                                        <img src={`./${post?.featured}`} alt="headline-pic" />
                                     </div>
-                                )}
+                                </div>
+
+                                <div className="p-2 text-lg">
+                                    <span>{post?.intro_text}</span>
+                                    {/* <span dangerouslySetInnerHTML={{ __html: post?.full_text }}></span> */}
+                                </div>
+
+                                <div className="flex justify-between items-center pt-2 pb-4">
+                                    <div className="w-1/2">
+                                        {post?.tags && (
+                                            <div className="font-thin">
+                                                {post?.tags.split(',').map((tag, index) => (
+                                                    <div className="badge rounded-pill bg-secondary me-1" key={index}>
+                                                        {tag}
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            </>
+                        )}
+
+                        {/* Render pdf file */}
+                        {(post?.content_type_id === 2 && post?.urls) && (
+                            <div className="px-2 pt-2 pb-4 flex justify-center">
+                                <object
+                                    data={post?.urls}
+                                    type="application/pdf"
+                                    width="100%"
+                                    height="720px"
+                                >
+                                    <p>Unable to display PDF file.<a href={post?.urls} target="_blank" className="ml-2 underline">Download</a> instead.</p>
+                                </object>
                             </div>
-                        </div>
+                        )}
+
+                        {/* Render image */}
+                        {(post?.content_type_id === 3 && post?.featured) && (
+                            <div className="p-2 mt-4 flex justify-center">
+                                <div className="w-3/5 rounded-md overflow-hidden">
+                                    <img src={`./${post?.featured}`} alt="headline-pic" />
+                                </div>
+                            </div>
+                        )}
                     </>
                 )}
-
-                {/* Render pdf file */}
-                {(news?.content_type_id === 2 && news?.urls) && (
-                    <div className="px-2 pt-2 pb-4 flex justify-center">
-                        <object
-                            data={news?.urls}
-                            type="application/pdf"
-                            width="100%"
-                            height="720px"
-                        >
-                            <p>Unable to display PDF file.<a href={news?.urls} target="_blank" className="ml-2 underline">Download</a> instead.</p>
-                        </object>
-                    </div>
-                )}
-
-                {/* Render image */}
-                {(news?.content_type_id === 3 && news?.featured) && (
-                    <div className="p-2 mt-4 flex justify-center">
-                        <div className="w-3/5 rounded-md overflow-hidden">
-                            <img src={`./${news?.featured}`} alt="headline-pic" />
-                        </div>
-                    </div>
-                )}
-
             </div>
         </section>
     )
