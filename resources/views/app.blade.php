1ad8fe9c7120cce2d4e84fbb4696bd150f7a3d36
sanyath007 <sanyath007@gmail.com>
2024-02-01 09:46:51 +0700

    feat(app.blade.php): add facebook messager plugin and SDK code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,5 +33,31 @@
         
         <!-- JS -->
         <script src="{{ asset('js/app.js') }}"></script>
+
+        <!-- Messenger ปลั๊กอินแชท Code -->
+        <div id="fb-customer-chat" class="fb-customerchat"></div>
+        <script>
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "766919693659033");
+            chatbox.setAttribute("attribution", "biz_inbox");
+        </script>
+
+        <!-- Your SDK code -->
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    xfbml   : true,
+                    version : 'v19.0'
+                });
+            };
+
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
     </body>
 </html>
