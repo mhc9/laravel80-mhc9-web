ba1c60fb2c129ab84cb4e5eb2a5400529d7578b9
sanyath007 <sanyath007@gmail.com>
2024-02-19 11:22:54 +0700

    reflactor: nable web routes and disable react frontend route and create new globals.css and main.js in public/css and public/js respectively

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Auth::routes();
+Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::view('/{path?}', 'app');
+/** Run into react fronend */
+// Route::view('/{path?}', 'app');
