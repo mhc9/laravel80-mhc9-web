08591f1c5843f9dd14bd7b545ca7f16fb7ad04b7
sanyath007 <sanyath007@gmail.com>
2024-02-20 18:54:49 +0700

    reflactor(share-buttons component view): add new popupWindow JS funtion and use its instead of openSocialApp function

--- resources/views/components/share-buttons.blade.php
+++ resources/views/components/share-buttons.blade.php
@@ -3,24 +3,24 @@
     <div class="flex flex-row gap-3 text-xl">
         <a
             class="facebook text-primary"
-            href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.mhc9dmh.com%2Fnewweb%2Fpublic%2F%23%2Fposts%2F{{$id}}"
-            onclick="openSocialApp(this, event, 'facebook-share')"
+            href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.mhc9dmh.com%2Fnewweb%2Fpublic%2Fposts%2F{{$id}}"
+            onclick="popupWindow($(this).attr('href'), 'facebook-share', 500, 300)"
             target="_blank"
         >
             <i class="fab fa-facebook"></i>
         </a>
         <a
             class="twitter text-info"
-            href="http://twitter.com/share?text={{$title}}&url=https%3A%2F%2Fwww.mhc9dmh.com%2Fnewweb%2Fpublic%2F%23%2Fposts%2F{{$id}}&via=sanyath007"
-            onclick="openSocialApp(this, event, 'twitter-share')"
+            href="http://twitter.com/share?text={{$title}}&url=https%3A%2F%2Fwww.mhc9dmh.com%2Fnewweb%2Fpublic%2Fposts%2F{{$id}}&via=sanyath007"
+            onclick="popupWindow($(this).attr('href'), 'twitter-share', 500, 300)"
             target="_blank"
         >
             <i class="fab fa-twitter"></i>
         </a>
         <a
             class="line text-success"
-            href="https://social-plugins.line.me/lineit/share?url=https%3A%2F%2Fwww.mhc9dmh.com%2Fnewweb%2Fpublic%2F%23%2Fposts%2F{{$id}}"
-            onclick="openSocialApp(this, event, 'line-share')"
+            href="https://social-plugins.line.me/lineit/share?url=https%3A%2F%2Fwww.mhc9dmh.com%2Fnewweb%2Fpublic%2Fposts%2F{{$id}}"
+            onclick="popupWindow($(this).attr('href'), 'line-share', 500, 300)"
             target="_blank"
         >
             <i class="fab fa-line"></i>
@@ -28,9 +28,9 @@
     </div>
 </div>
 <script>
-    function openSocialApp(el, event, appName) {
-        event.preventDefault();
-        window.open($(el).attr('href'), appName, 'width=500, height=300');
-        return false;
+    function popupWindow(url, title, w, h) {
+        var y = window.outerHeight / 2 + window.screenY - (h / 2)
+        var x = window.outerWidth / 2 + window.screenX - (w / 2)
+        return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + y + ', left=' + x);
     }
 </script>
