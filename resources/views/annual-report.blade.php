dc473b2c7d75e478ef576feef7eae77c16e18e55
sanyath007 <sanyath007@gmail.com>
2024-02-21 13:05:25 +0700

    reflactor: re-design annual-report view

--- resources/views/annual-report.blade.php
+++ resources/views/annual-report.blade.php
@@ -9,18 +9,20 @@
     <div class="annual__report-wrapper px-4 pt-2 pb-4">
         <div class="row">
             @foreach($reports as $report)
-                <dvi class="col-md-3">
+                <dvi class="col-md-4">
                     <div class="card">
-                        <img src="./{{$report['featured']}}" class="card-img-top" alt="..." />
+                        <div class="card-img-top overflow-hidden">
+                            <img src="{{ url('/' . $report['featured']) }}" alt="report-cover" />
+                        </div>
                         <div class="card-body">
                             <div class="card-text text-xl font-bold mb-1">{{$report['title']}}</div>
                             <div class="text-muted text-xs font-thin flex items-center gap-2 mb-2">
-                                <span><i class="fas fa-calendar-alt"></i> {'2024-01-22 16.30'} น.</span>
-                                <span><i class="fas fa-user-alt"></i> {'Administrator'}</span>
-                                <span><i class="fas fa-eye"></i> {'0'}</span>
-                                <span><i class="fas fa-download"></i> {'0'}</span>
+                                <span><i class="fas fa-calendar-alt"></i> 2024-01-22 16.30 น.</span>
+                                <span><i class="fas fa-user-alt"></i> Administrator</span>
+                                <span><i class="fas fa-eye"></i> 0</span>
+                                <span><i class="fas fa-download"></i> 0</span>
                             </div>
-                            <div class="flex justify-center pt-4">
+                            <div class="flex justify-center pt-2">
                                 <a href="{{$report['urls']}}" target="_blank" class="btn btn-light">
                                     <i class="fas fa-download"></i> ดาวน์โหลด
                                 </a>
