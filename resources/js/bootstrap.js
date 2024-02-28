ac5c47483977dbbfdc12c1a7fa63a08d9ac672e8
sanyath007 <sanyath007@gmail.com>
2024-02-28 08:49:59 +0700

    reflactor: move code of swiper initial from bootstrap.js to app.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,5 @@
+window.API_URL = process.env.MIX_APP_URL;
+
 window._ = require('lodash');
 
 try {
@@ -31,93 +33,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: true
 // });
 
-window.API_URL = process.env.MIX_APP_URL;
-
 const { getWindowDimensions } = require('./utils');
 
 window.getWindowDimensions = getWindowDimensions;
-
-import { Swiper } from 'swiper'
-import { Autoplay, Navigation, Pagination } from 'swiper/modules'
-
-Swiper.use(Autoplay)
-
-var swiperHero = new Swiper("#hero", {
-    modules: [Navigation, Pagination],
-    autoplay: {
-        delay: 3000,
-    },
-    cssMode: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-    loop: true,
-});
-
-var swiperOnepage = new Swiper("#onepage", {
-    modules: [Navigation, Pagination],
-    autoplay: {
-        delay: 3000,
-    },
-    slidesPerView: 4,
-    spaceBetween: 30,
-    cssMode: true,
-    mousewheel: true,
-    keyboard: true,
-    loop: true,
-    breakpoints: {
-        375: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        430: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        992: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
-        // 1024: {
-        //     slidesPerView: 5,
-        //     spaceBetween: 50,
-        // },
-    },
-});
-
-var swiperPartner = new Swiper("#partner", {
-    modules: [Navigation, Pagination],
-    autoplay: {
-        delay: 3000,
-    },
-    slidesPerView: 4,
-    spaceBetween: 30,
-    cssMode: true,
-    mousewheel: true,
-    keyboard: true,
-    loop: true,
-    breakpoints: {
-        375: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        430: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        992: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
-        // 1024: {
-        //     slidesPerView: 5,
-        //     spaceBetween: 50,
-        // },
-    },
-});
