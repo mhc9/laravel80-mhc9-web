34c9506b3a9513bccdd34feb24ca4e151a7afb3a
sanyath007 <sanyath007@gmail.com>
2024-03-04 14:14:48 +0700

    reflactor: reset css classes in sat-servey view

--- resources/views/e-report/sat-servey.blade.php
+++ resources/views/e-report/sat-servey.blade.php
@@ -8,13 +8,13 @@
 
     <div class="content-wrapper">
         <a href="{{ url('/posts/88') }}">
-            <div class="policy">
+            <div class="content-bullet">
                 <h3>สรุปผลความพึงพอใจของผู้รับบริการต่อการบริการ (ผู้รับบริการ)</h3>
                 <span><i class="fas fa-angle-down"></i></span>
             </div>
         </a>
         <a href="{{ url('/posts/89') }}">
-            <div class="policy">
+            <div class="content-bullet">
                 <h3>สรุปผลความพึงพอใจของผู้รับบริการต่อการบริการ (เครือข่าย สธ.)</h3>
                 <span><i class="fas fa-angle-down"></i></span>
             </div>
