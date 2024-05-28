c192c662889ffdd5c7244768b520f11f1f0231c5
sanyath007 <sanyath007@gmail.com>
2024-05-28 11:09:32 +0700

    reflactor(policies/pdpa view): change page header text

--- resources/views/policies/pdpa.blade.php
+++ resources/views/policies/pdpa.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="content-container container">
-    <h1 class="title">นโยบายองค์กร</h1>
+    <h1 class="title">นโยบายด้านการคุ้มครองข้อมูลส่วนบุคคล (PDPA)</h1>
 
     <hr class="my-2" />
 
