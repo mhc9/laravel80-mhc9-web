00ea0902a327aaecd2067ea76862f7c7ee3df73a
sanyath007 <sanyath007@gmail.com>
2024-12-26 09:42:18 +0700

    reflactor(social-icons component): update url of social media button

--- resources/views/components/social-icons.blade.php
+++ resources/views/components/social-icons.blade.php
@@ -2,13 +2,13 @@
     <a href="https://www.facebook.com/profile.php?id=100064648815470" target="_blank" rel="noreferrer" class="hover:text-gray-300">
         <i class="fab fa-facebook"></i>
     </a>
-    <a href="https://twitter.com/fN0pnkdyGrckiLh" target="_blank" rel="noreferrer" class="hover:text-gray-300">
+    <a href="https://x.com/dmh_mhc9" target="_blank" rel="noreferrer" class="hover:text-gray-300">
         <i class="fab fa-twitter"></i>
     </a>
     <a href="https://www.instagram.com/mentalhealthcenter_9" target="_blank" rel="noreferrer" class="hover:text-gray-300">
         <i class="fab fa-instagram"></i>
     </a>
-    <a href="https://www.youtube.com/@user-vf2ep3tx6h" target="_blank" rel="noreferrer" class="hover:text-gray-300">
+    <a href="https://www.youtube.com/@%E0%B8%A8%E0%B8%B9%E0%B8%99%E0%B8%A2%E0%B9%8C%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%88%E0%B8%B4%E0%B8%95%E0%B8%97%E0%B8%B5%E0%B9%889%E0%B8%99%E0%B8%84%E0%B8%A3%E0%B8%A3%E0%B8%B2%E0%B8%8A%E0%B8%AA%E0%B8%B5%E0%B8%A1%E0%B8%B2" target="_blank" rel="noreferrer" class="hover:text-gray-300">
         <i class="fab fa-youtube"></i>
     </a>
     <a href="https://line.me/ti/p/@229xqzio" target="_blank" rel="noreferrer" class="hover:text-gray-300">
