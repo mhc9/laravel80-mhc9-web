19721ff29c163c5a3deff753e5a69514d15431e7
sanyath007 <sanyath007@gmail.com>
2025-01-15 15:53:59 +0700

    reflactor: update e-report/corruption view

--- resources/views/e-report/corruption.blade.php
+++ resources/views/e-report/corruption.blade.php
@@ -10,54 +10,89 @@
 
     <div class="content-wrapper p-4">
         <div class="min-h-screen">
+            <div class="flex flex-col justify-center items-center mb-2">
+                <h3 class="text-center">รายงานสถิติเรื่องร้องเรียนการทุจริตหรือประพฤติมิชอบของเจ้าหน้าที่<br />ศูนย์สุขภาพจิตที่ 9 กรมสุขภาพจิต</h3>
+                <h5>(ประจำปีงบประมาณ 2568 (ข้อมูลตั้งแต่ 1 ตุลาคม 2567- 28 กุมภาพันธ์ 2568)</h5>
+            </div>
+
             <table class="table table-bordered table-striped table-hover text-lg">
                 <thead>
                     <tr>
-                        <th class="text-center">ประจำเดือน</th>
-                        <th class="text-center w-[22%]">จำนวนข้อร้องเรียน</th>
-                        <th class="text-center w-[22%]">จำนวนเรื่องที่อยู่ระหว่างดำเนินการ</th>
-                        <th class="text-center w-[22%]">จำนวนเรื่องที่ดำเนินการแล้วเสร็จ</th>
+                        <th class="text-center" rowspan="2">ประจำเดือน</th>
+                        <th class="text-center" colspan="2">จำนวนข้อร้องเรียนที่รับไว้พิจารณา</th>
+                        <th class="text-center w-[10%]" rowspan="2">จำนวนเรื่องร้องเรียน<br />ทั้งหมด</th>
+                        <th class="text-center w-[10%]" rowspan="2">จำนวนเรื่องที่<br />ดำเนินการแล้วเสร็จ</th>
+                        <th class="text-center w-[10%]" rowspan="2">จำนวนเรื่องที่<br />อยู่ระหว่างดำเนินการ</th>
+                        <th class="text-center w-[15%]" rowspan="2">หมายเหตุ</th>
+                    </tr>
+                    <tr>
+                        <th class="text-center w-[10%]">ทุจริตต่อหน้าที่</th>
+                        <th class="text-center w-[10%]">ประพฤติมิชอบ</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td class="text-center">ตุลาคม 2566</td>
+                        <td class="text-center">ตุลาคม 2567</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">&nbsp;</td>
                     </tr>
                     <tr>
-                        <td class="text-center">พฤศจิกายน 2566</td>
+                        <td class="text-center">พฤศจิกายน 2567</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">&nbsp;</td>
                     </tr>
                     <tr>
-                        <td class="text-center">ธันวาคม 2566</td>
+                        <td class="text-center">ธันวาคม 2567</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">&nbsp;</td>
                     </tr>
                     <tr>
-                        <td class="text-center">มกราคม 2567</td>
+                        <td class="text-center">มกราคม 2568</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">&nbsp;</td>
                     </tr>
                     <tr>
-                        <td class="text-center">กุมภาพันธ์ 2567</td>
+                        <td class="text-center">กุมภาพันธ์ 2568</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">&nbsp;</td>
                     </tr>
                     <tr>
                         <td class="text-center">รวม</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
                         <td class="text-center">0</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">-</td>
+                        <td class="text-center">&nbsp;</td>
                     </tr>
                 </tbody>
             </table>
+
+            <div>
+                <b class="underline">หมายเหตุ</b>
+                <p>“ทุจริตต่อหน้าที่” หมายความว่า ปฏิบัติหรือละเว้นการปฏิบัติอย่างใดในตำแหน่งหรือหน้าที่ หรือปฏิบัติหรือละเว้นการปฏิบัติอย่างใดในพฤติการณ์ที่อาจทำให้ผู้อื่นเชื่อว่ามีตำแหน่งหรือหน้าที่ ทั้งที่ตนมิได้ มีตำแหน่งหรือหน้าที่นั้น หรือใช้อำนาจในตำแหน่งหรือหน้าที่ ทั้งเพื่อแสวงหาผลประโยชน์ที่มิควรได้โดยชอบ สำหรับตนเองหรือผู้อื่น หรือกระทำการอันเป็นความผิดต่อตำแหน่งหน้าที่ราชการ หรือความผิดต่อตำแหน่ง หน้าที่ในการยุติธรรมตามประมวลกฎหมายอาญาหรือตามกฎหมายอื่น</p>
+                <p>“ประพฤติมิชอบ” หมายความว่า การใช้อำนาจในตำแหน่งหรือหน้าที่อันเป็นการฝ่าฝืน กฎหมาย ระเบียบ คำสั่ง หรือมติคณะรัฐมนตรีที่มุ่งหมายจะควบคุมดูแลการรับ การเก็บรักษา หรือการใช้เงิน หรือทรัพย์สินของแผ่นดินซึ่งมิใช่การทุจริตต่อหน้าที่</p>
+            </div>
         </div>
     </div>
 </section>
