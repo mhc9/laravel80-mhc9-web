9359fa975bc3d87be42d81855fff1ef086e42df9
sanyath007 <sanyath007@gmail.com>
2025-01-17 09:49:25 +0700

    reflactor: add new Facebook chat plugin  to layouts/default view

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -52,28 +52,30 @@
         // });
     </script>
 
-    <!-- Facebook SDK -->
-    <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v19.0" nonce="3W0TiTA5"></script>
-    <!-- Facebook SDK -->
-
-    <!-- LINE Plugins -->
+    <!-- ================== LINE Plugins ================== -->
     <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
-    <!-- LINE Plugins -->
-    
+    <!-- ================== LINE Plugins ================== -->
 
-    <!-- Messenger ปลั๊กอินแชท Code -->
-    <div id="fb-customer-chat" class="fb-customerchat"></div>
+    <!-- ================== Facebook ปลั๊กอิน ================== -->
+    <!-- option 1 -->
+    <!-- <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v19.0" nonce="3W0TiTA5"></script> -->
+    
+    <!-- option 2 -->
+    <!-- <div id="fb-customer-chat" class="fb-customerchat"></div>
     <script>
         var chatbox = document.getElementById('fb-customer-chat');
         chatbox.setAttribute("page_id", "766919693659033");
         chatbox.setAttribute("attribution", "biz_inbox");
-    </script>
+    </script> -->
 
-    <!-- Your SDK code -->
+    <!-- option 3 -->
+    <div class="fb-customerchat" page_id="766919693659033" logged_in_greeting="สอบถามเพิ่มเติม ?" logged_out_greeting="สอบถามเพิ่มเติม"></div>
     <script>
         window.fbAsyncInit = function() {
             FB.init({
+                // appId: '<APP_ID>',
+                autoLogAppEvents: true,
                 xfbml   : true,
                 version : 'v19.0'
             });
@@ -82,14 +84,18 @@
         (function(d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
+            js = d.createElement(s);
+            js.id = id;
             js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     </script>
+    <!-- ================== Facebook ปลั๊กอิน ================== -->
 
+    <!-- ================== GPPC ================== -->
     <!-- Cookie Consent of GPPC -->
     <script src="https://gppc-app.onde.go.th/cdn/pdpa-cookieconsent.js"></script>
     <script src="https://gppc-app.onde.go.th/cdn/pdpa-cookieconsent-init.js?client=ca-pub-311584378020240521&adjust=true"></script>
+    <!-- ================== GPPC ================== -->
 </body>
 </html>
