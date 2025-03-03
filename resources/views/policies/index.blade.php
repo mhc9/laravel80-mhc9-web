82d16ad0f422b06d5d33477303367f3d6856dd7b
sanyath007 <sanyath007@gmail.com>
2025-03-03 16:09:24 +0700

    reflactor(policies view): add new item

--- resources/views/policies/index.blade.php
+++ resources/views/policies/index.blade.php
@@ -10,13 +10,19 @@
         <div class="min-h-screen px-4">
             <a href="./posts/132">
                 <div class="content-bullet">
-                    <h3>นโยบายการปฏิบัติงานศูนย์สุขภาพจิตที่ 9</h3>
+                    <h4 class="text-[16px]">นโยบายการปฏิบัติงานศูนย์สุขภาพจิตที่ 9</h4>
                     <span><i class="fas fa-angle-down"></i></span>
                 </div>
             </a>
             <a href="{{ url('/policies/pdpa') }}">
                 <div class="content-bullet">
-                    <h3>นโยบายด้านการคุ้มครองข้อมูลส่วนบุคคล (PDPA)</h3>
+                    <h4 class="text-[16px]">นโยบายด้านการคุ้มครองข้อมูลส่วนบุคคล (PDPA)</h4>
+                    <span><i class="fas fa-angle-down"></i></span>
+                </div>
+            </a>
+            <a href="{{ url('./posts/311') }}">
+                <div class="content-bullet">
+                    <h4 class="text-[16px]">นโยบายและแนวปฏิบัติการรักษาความมั่นคงปลอดภัยไซเบอร์</h4>
                     <span><i class="fas fa-angle-down"></i></span>
                 </div>
             </a>
