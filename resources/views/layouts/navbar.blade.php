f04a8ac4305246ecd1193763ddd05bd88e0c5fad
sanyath007 <sanyath007@gmail.com>
2025-10-27 09:48:31 +0700

    reflactor: update new logo

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,12 +6,9 @@
                     <div class="menu-wrapper">
                         <!-- Logo -->
                         <a href="{{ url('/') }}" class="nav-logo">
-                            <picture class="logo">
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/f/f9/%E0%B8%95%E0%B8%A3%E0%B8%B2%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%87%E0%B8%AA%E0%B8%B2%E0%B8%98%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88.png"
-                                    alt="logo"
-                                />
-                            </picture>
+                            <div class="logo">
+                                <img src="{{ asset('img/logo_black.jpg') }}" alt="logo" />
+                            </div>
                             <div class="logo-text">
                                 <h2>ศูนย์สุขภาพจิตที่ 9</h2>
                                 <p>Mental Health Center 9</p>
