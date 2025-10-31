c8b742bc566d83b9014a459c03b7fa3defefd315
sanyath007 <sanyath007@gmail.com>
2025-10-31 11:20:28 +0700

    reflactor: add new Weekly calendar section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -665,7 +665,7 @@
     </section>
 
     <!-- Weekly calendar -->
-    <!-- <section>
+    <section>
         <div class="container p-4">
             <h1 class="title">
                 <i class="fas fa-calendar"></i>
@@ -679,17 +679,17 @@
                     src="https://app.mhc9dmh.com/calendar"
                     title="กิจกรรมประจำสัปดาห์"
                     style="position: absolute; top: 0; left: 0; bottom: 0; right: 0; width: 100%; height: 100%;"
-                ></iframe> -->
+                ></iframe>
 
                 <!-- Floating button -->
-                <!-- <div class="absolute top-2 left-2 flex justify-center items-center z-10 w-[50px] h-[50px] bg-indigo-600 hover:bg-indigo-800 backdrop-blur-lg rounded-full border-2 text-white shadow-lg p-3">
+                <div class="absolute top-2 left-2 flex justify-center items-center z-10 w-[50px] h-[50px] bg-indigo-600 hover:bg-indigo-800 backdrop-blur-lg rounded-full border-2 text-white shadow-lg p-3">
                     <a href="https://app.mhc9dmh.com/calendar" target="_blank" class="transition-all duration-300 hover:scale-110">
                         <i class="fas fa-expand text-xl mt-1"></i>
                     </a>
                 </div>
             </div>
         </div>
-    </section> -->
+    </section>
 
     <!-- Partners Logo --> 
     <section class="slide-logos">
