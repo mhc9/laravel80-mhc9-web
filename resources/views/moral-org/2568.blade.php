0281a9286449efb10ad4f8a309794089847715b3
sanyath007 <sanyath007@gmail.com>
2025-11-20 05:02:25 +0700

    feat: create new moral-org's index page

---
+++ resources/views/moral-org/2568.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.default')
+
+@section('content')
+<article class="container">
+    <h1 class="title">องค์กรคุณธรรมต้นแบบ</h1>
+
+    <hr class="my-2" />
+
+    <div class="content-wrapper">
+        <div class="min-h-screen p-4">
+            <div class="text-center">
+                <h2 class="text-2xl font-bold mt-2 mb-8">การดำเนินการเป็นองค์กรคุณธรรมต้นแบบ ประจำปีงบประมาณ พ.ศ. 2568 ของศูนย์สุขภาพจิตที่ 9 กรมสุขภาพจิต</h2>
+            </div>
+
+            <div>
+                <h2 class="text-xl">1. ประกาศเจตนารมณ์ของผู้บริหารและบุคลากรขององค์กร</h2>
+
+                <a href="{{ url('/posts/210') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">1.1 ประกาศเจตนารมณ์ร่วมกันในการขับเคลื่อนหน่วยงานให้เป็นองค์กรคุณธรรมต้นแบบ</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/posts/211') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">1.2 ประกาศเจตนารมณ์ร่วมกันในการต่อต้านการทุจริตและไม่รับของขวัญและของกำนัลทุกชนิดจากการปฏิบัติหน้าที่ (No Gift Policy)</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/posts/212') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">1.3 ประกาศเจตนารมณ์การป้องกันและแก้ไขปัญหาการแสวงหาประโยชน์ทางเพศ และการคุกคามทางเพศในการทำงาน</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">2. กำหนดคุณธรรมเป้าหมาย</h2>
+
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">การกำหนดคุณธรรมเป้าหมายจาก “ปัญหาที่อยากแก้” และ “ความดีที่อยากทำ”</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">3. แผนการดำเนินงานตามเป้าหมาย</h2>
+
+                <a href="{{ url('/posts/213') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">3.1 คำสั่งแต่งตั้งคณะทำงานจัดทำและขับเคลื่อนแผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">3.2 แผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">4. ผลสำเร็จของการดำเนินกิจกรรมตามแผนปฏิบัติการฯ</h2>
+
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">4.1 รายงานผลการดำเนินการตามแผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน รอบ 6 เดือน</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">4.2 รายงานผลสำเร็จของการดำเนินการกิจกรรมตามแผนปฏิบัติการฯ (ไม่น้อยกว่าร้อยละ 70)</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">5. กิจกรรมถอดบทเรียนจากาการดำเนินงานองค์กรคุณธรรมต้นแบบ</h2>
+
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">5.1 กิจกรรมถอดบทเรียนการดำเนินงานเป็นองค์กรคุณธรรมต้นแบบ</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">5.2 การปรับแผนปฏิบัติการส่งเสริมคุณธรรมฯ</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">6. กิจกรรมยกย่อง เชิดชู บุคลากร และกลุ่ม/กลุ่มงาน ที่ทำความดีเป็นแบบอย่าง</h2>
+
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">6.1 กิจกรรมยกย่อง เชิดชู บุคลากร และหน่วยงานภายในองค์กร</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">6.2 การประชาสัมพันธ์ผ่านช่องทางการสื่อสาร</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">7. ผลสำเร็จของการดำเนินงาน</h2>
+
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">7.1 รายงานผลสำเร็จของการดำเนินกิจกรรมตามแผนปฏิบัติการฯ (ไม่น้อยกว่าร้อยละ 70)</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">7.2 รายงานผลการประเมินพฤติกรรมที่เปลี่ยนแปลงตามคุณธรรมเป้าหมาย 5 ประการ พอเพียง วินัย สุจริต จิตอาสา กตัญญู</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">8. องค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรม</h2>
+
+                <a href="{{ url('/posts/387') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">8.1 เอกสารองค์ความรู้หรือผลกสำเร็จการดำเนินกิจกรรมตามแผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">8.2 ผลการประเมินองค์กรคุณธรรมต้นแบบของหน่วยงาน</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">9. เอกสารองค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรม</h2>
+
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">9.1 กิจกรรมเผยแพร่เอกสารองค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรมผ่านช่องทางการสื่อสาร</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">9.2 รายงานผลการดำเนินการตามแผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน รอบ 12 เดือน</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</article>
+@endsection
--- resources/views/moral-org/index.blade.php
+++ resources/views/moral-org/index.blade.php
@@ -1,68 +1,82 @@
 @extends('layouts.default')
 
 @section('content')
-<section class="content-container container">
+<article class="container">
     <h1 class="title">องค์กรคุณธรรมต้นแบบ</h1>
 
     <hr class="my-2" />
 
     <div class="content-wrapper">
-        <div class="min-h-screen px-4 mb-4">
-            <div class="text-lg text-center font-bold mb-3">การดำเนินการเป็นองค์กรคุณธรรมต้นแบบ ประจำปีงบประมาณ พ.ศ. 2568 ของศูนย์สุขภาพจิตที่ 9 กรมสุขภาพจิต</div>
+        <div class="min-h-screen p-4">
+            <div class="text-center">
+                <h2 class="text-2xl font-bold mt-2 mb-8">การดำเนินการเป็นองค์กรคุณธรรมต้นแบบ ประจำปีงบประมาณ พ.ศ. 2569 ของศูนย์สุขภาพจิตที่ 9 กรมสุขภาพจิต</h2>
+            </div>
 
             <div>
-                <h2 class="text-xl">1. ประกาศเจตนารมณ์ของผู้บริหารและบุคลากรขององค์กร</h2>
+                <h2 class="text-xl">1. ผู้บริหารองค์กรและบุคลากรในหน่วยงานไม่น้อยกว่าร้อยละ 80 ร่วมกันประกาศเจตนารมณ์เป็นลายลักษณ์อักษร ดังนี้</h2>
 
-                <a href="{{ url('/posts/210') }}">
+                <a href="{{ url('/') }}">
                     <div class="content-bullet">
                         <h4 class="text-[16px]">1.1 ประกาศเจตนารมณ์ร่วมกันในการขับเคลื่อนหน่วยงานให้เป็นองค์กรคุณธรรมต้นแบบ</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
-                <a href="{{ url('/posts/211') }}">
+                <a href="{{ url('/') }}">
                     <div class="content-bullet">
                         <h4 class="text-[16px]">1.2 ประกาศเจตนารมณ์ร่วมกันในการต่อต้านการทุจริตและไม่รับของขวัญและของกำนัลทุกชนิดจากการปฏิบัติหน้าที่ (No Gift Policy)</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
-                <a href="{{ url('/posts/212') }}">
+                <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">1.3 ประกาศเจตนารมณ์การป้องกันและแก้ไขปัญหาการแสวงหาประโยชน์ทางเพศ และการคุกคามทางเพศในการทำงาน</h4>
+                        <h4 class="text-[16px]">1.3 ประกาศเจตนารมณ์การป้องกันและแก้ไขปัญหาการล่วงละเมิด และการคุกคามทางเพศในการทำงาน</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
             </div>
 
             <div class="mt-4">
-                <h2 class="text-xl">2. กำหนดคุณธรรมเป้าหมาย</h2>
+                <h2 class="text-xl">2. การกำหนดคุณธรรมเป้าหมายจาก “ปัญหาที่อยากแก้” และ “ความดีที่อยากทำ” และปฏิญญาคุณธรรม (Do & Don’t)</h2>
 
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">การกำหนดคุณธรรมเป้าหมายจาก “ปัญหาที่อยากแก้” และ “ความดีที่อยากทำ”</h4>
-                        <span><i class="fas fa-angle-down"></i></span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="mt-4">
-                <h2 class="text-xl">3. แผนการดำเนินงานตามเป้าหมาย</h2>
-
-                <a href="{{ url('/posts/213') }}">
-                    <div class="content-bullet">
-                        <h4 class="text-[16px]">3.1 คำสั่งแต่งตั้งคณะทำงานจัดทำและขับเคลื่อนแผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน</h4>
+                        <h4 class="text-[16px]">2.1 กำหนดปัญหาที่อยากแก้ ความดีที่อยากทำ ไม่น้อยกว่า 5 เรื่อง</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">3.1 แผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน</h4>
+                        <h4 class="text-[16px]">2.2 กำหนดปฏิญญาคุณธรรม (ขั้นต้น) (Do & Don’t) ไม่น้อยกว่า 3 เรื่อง</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
             </div>
 
             <div class="mt-4">
-                <h2 class="text-xl">4. ผลสำเร็จของการดำเนินกิจกรรมตามแผนปฏิบัติการฯ</h2>
+                <h2 class="text-xl">3. การจัดทำแผนการดำเนินงานตามเป้าหมายที่กำหนดไว้ในข้อที่ 2</h2>
+
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">3.1 คำสั่งแต่งตั้งคณะทำงานจัดทำและขับเคลื่อนแผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน ประจำปีงบประมาณ พ.ศ. 2569</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">3.2 แผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน ประจำปีงบประมาณ พ.ศ. 2569</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">3.3 การดำเนินกิจกรรมการขับเคลื่อนคุณธรรม “ชีวิตดีวิถีพอเพียง”</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">4. องค์กรมีผลสำเร็จของการดำเนินกิจกรรมตามแผนปฏิบัติการฯ (ไม่น้อยกว่าร้อยละ 70)</h2>
 
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
@@ -72,68 +86,99 @@
                 </a>
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">4.2 รายงานผลสำเร็จของการดำเนินการกิจกรรมตามแผนปฏิบัติการฯ (ไม่น้อยกว่าร้อยละ 70)</h4>
+                        <h4 class="text-[16px]">4.2 รายงานผลสำเร็จของการดำเนินกิจกรรมตามแผนปฏิบัติการฯ (ไม่น้อยกว่าร้อยละ 70)</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
             </div>
 
             <div class="mt-4">
-                <h2 class="text-xl">5. กิจกรรมถอดบทเรียนจากาการดำเนินงานองค์กรคุณธรรมต้นแบบ</h2>
+                <h2 class="text-xl">5. กิจกรรมถอดบทเรียนจากการดำเนินงานองค์กรคุณธรรมต้นแบบของกรมสุขภาพจิต</h2>
 
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">5.1 กิจกรรมถอดบทเรียนการดำเนินงานเป็นองค์กรคุณธรรมต้นแบบ</h4>
+                        <h4 class="text-[16px]">5.1 การดำเนินการ “ปฏิญญาคุณธรรม (Do & Don’t)” หลังได้ทดลองปฏิบัติแล้วไม่น้อยกว่า 3 เดือน</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">5.2 การปรับแผนปฏิบัติการส่งเสริมคุณธรรมฯ</h4>
+                        <h4 class="text-[16px]">5.2 กิจกรรมถอดบทเรียนการดำเนินงานเป็นองค์กรคุณธรรมต้นแบบ</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
-            </div>
-
-            <div class="mt-4">
-                <h2 class="text-xl">6. กิจกรรมยกย่อง เชิดชู บุคลากร และกลุ่ม/กลุ่มงาน ที่ทำความดีเป็นแบบอย่าง</h2>
-
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">6.1 กิจกรรมยกย่อง เชิดชู บุคลากร และหน่วยงานภายในองค์กร</h4>
-                        <span><i class="fas fa-angle-down"></i></span>
-                    </div>
-                <a href="{{ url('/') }}">
-                    <div class="content-bullet">
-                        <h4 class="text-[16px]">6.1 การประชาสัมพันธ์ผ่านช่องทางการสื่อสาร</h4>
+                        <h4 class="text-[16px]">5.3 การปรับแผนปฏิบัติการส่งเสริมคุณธรรมฯ (ถ้ามี)</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
             </div>
 
             <div class="mt-4">
-                <h2 class="text-xl">7. ผลสำเร็จของการดำเนินงาน</h2>
+                <h2 class="text-xl">6. กิจกรรมยกย่อง เชิดชู บุคลากร และกลุ่ม / กลุ่มงาน ที่ทำความดีจนเป็นแบบอย่างได้</h2>
 
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">7.1 รายงานผลสำเร็จของการดำเนินกิจกรรมตามแผนปฏิบัติการฯ (ไม่น้อยกว่าร้อยละ 70)</h4>
+                        <h4 class="text-[16px]">6.1 กิจกรรมยกย่อง เชิดชู บุคลากร</h4>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">7.2 รายงานผลการประเมินพฤติกรรมที่เปลี่ยนแปลงตามคุณธรรมเป้าหมาย 5 ประการ พอเพียง วินัย สุจริต จิตอาสา กตัญญู</h4>
+                        <h4 class="text-[16px]">6.2 กิจกรรมยกย่อง เชิดชู หน่วยงานภายในองค์กร</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <div class="w-full">
+                            <h4 class="text-[16px]">6.3 การประชาสัมพันธ์ผ่านช่องทางการสื่อสาร</h4>
+                            <ul class="m-0">
+                                <li class="m-0"><a href="{{ url('/') }}">ช่องทางเว็บไซต์หน่วยงาน</a></li>
+                                <li class="m-0"><a href="{{ url('/') }}">ช่องทาง Facebook</a></li>
+                            </ul>
+                        </div>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
             </div>
 
             <div class="mt-4">
-                <h2 class="text-xl">8. องค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรม</h2>
+                <h2 class="text-xl">7. ผลสำเร็จของการดำเนินงานตามเป้าหมายของแผนการดำเนินงานที่กำหนดไว้ในข้อที่ 3 เพิ่มมากขึ้น</h2>
 
-                <a href="{{ url('/posts/387') }}">
+                <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">8.1 เอกสารองค์ความรู้หรือผลกสำเร็จการดำเนินกิจกรรมตามแผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน</h4>
+                        <h4 class="text-[16px]">7.1 รายงานผลสำเร็จของการดำเนินกิจกรรมตามแผนปฏิบัติการฯ (ไม่น้อยกว่าร้อยละ 90)</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">7.2 ผู้บริหารดำเนินการประเมินกระบวนการดำเนินงานตามแผนและพฤติกรรมที่เปลี่ยนแปลง</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <h4 class="text-[16px]">7.3 รายงานผลการประเมินพฤติกรรมที่เปลี่ยนแปลงตามคุณธรรมเป้าหมาย 5 ประการ พอเพียง วินัย สุจริต จิตอาสา กตัญญู</h4>
+                        <span><i class="fas fa-angle-down"></i></span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h2 class="text-xl">8. การรวบรวมองค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรมตามแผนการดำเนินงานฯ</h2>
+
+                <a href="{{ url('/') }}">
+                    <div class="content-bullet">
+                        <div class="w-full">
+                            <h4 class="text-[16px]">8.1 เอกสารองค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรมตามแผนปฏิบัติการส่งเสริมคุณธรรมของหน่วยงาน</h4>
+                            <ul class="m-0">
+                                <li class="m-0"><a href="{{ url('/') }}">คลิปวิดีโอ</a></li>
+                                <li class="m-0"><a href="{{ url('/') }}">Powerpoint</a></li>
+                            </ul>
+                        </div>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
@@ -146,11 +191,18 @@
             </div>
 
             <div class="mt-4">
-                <h2 class="text-xl">9. เอกสารองค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรม</h2>
+                <h2 class="text-xl">9. เผยแพร่เอกสารองค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรมและมีความพร้อมเป็นแหล่งเรียนรู้ให้กับองค์กรอื่นๆ ได้</h2>
 
                 <a href="{{ url('/') }}">
                     <div class="content-bullet">
-                        <h4 class="text-[16px]">9.1 กิจกรรมเผยแพร่เอกสารองค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรมผ่านช่องทางการสื่อสาร</h4>
+                        <div class="w-full">
+                            <h4 class="text-[16px]">9.1 กิจกรรมเผยแพร่เอกสารองค์ความรู้หรือผลสำเร็จการดำเนินกิจกรรมผ่านช่องทางการสื่อสาร</h4>
+                            <ul class="m-0">
+                                <li class="m-0"><a href="{{ url('/') }}">ช่องทางเว็บไซต์หน่วยงาน</a></li>
+                                <li class="m-0"><a href="{{ url('/') }}">ช่องทาง Facebook</a></li>
+                                <li class="m-0"><a href="{{ url('/') }}">ช่องทาง Line</a></li>
+                            </ul>
+                        </div>
                         <span><i class="fas fa-angle-down"></i></span>
                     </div>
                 </a>
@@ -163,5 +215,5 @@
             </div>
         </div>
     </div>
-</section>
+</article>
 @endsection
